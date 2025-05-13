f9468633d3f83974f29d6dfe249f5df1682d2d0e
Robert Isaacs <robert@nineminds.com>
2025-05-13 12:01:32 -0400

    feat: Add E2E Tests
    *   **CI/CD:**
        *   Added a new GitHub Actions workflow (`.github/workflows/e2e-fresh-install-tests.yaml`) to perform end-to-end tests on fresh installations, improving automated quality assurance.

--- server/src/lib/secrets/FileSystemSecretProvider.ts
+++ server/src/lib/secrets/FileSystemSecretProvider.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
-import { ISecretProvider } from './ISecretProvider';
+import { ISecretProvider } from './ISecretProvider.js';
 // import logger from 'server/src/utils/logger'; // Corrected logger import path again
 
 /**
