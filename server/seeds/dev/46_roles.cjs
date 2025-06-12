fb64c8b0186756625292e7c66d67ee3fefa81952
Natallia Bukhtsik <natallia@nineminds.com>
2025-06-12 13:06:46 -0400

    and a seed

--- server/seeds/dev/46_roles.cjs
+++ server/seeds/dev/46_roles.cjs
@@ -5,8 +5,9 @@ exports.seed = function (knex) {
                 { tenant: knex('tenants').select('tenant').first(), role_name: 'Admin', description: 'Full system access' },
                 { tenant: knex('tenants').select('tenant').first(), role_name: 'Manager', description: 'Manage tickets and users' },
                 { tenant: knex('tenants').select('tenant').first(), role_name: 'Technician', description: 'Handle tickets' },
-                { tenant: knex('tenants').select('tenant').first(), role_name: 'client', description: 'Client user role' },
-                { tenant: knex('tenants').select('tenant').first(), role_name: 'client_admin', description: 'Client administrator role' }
+                { tenant: knex('tenants').select('tenant').first(), role_name: 'Client', description: 'Client user role' },
+                { tenant: knex('tenants').select('tenant').first(), role_name: 'Client_Admin', description: 'Client administrator role' },
+                { tenant: knex('tenants').select('tenant').first(), role_name: 'Dispatcher', description: 'Role for users who can dispatch and schedule for other users' }
             ]);
         });
 };
