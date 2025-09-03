3e5be4f946ee6b1ae09c4d052bb3119dcb53f625
Natallia Bukhtsik <natallia@nineminds.com>
2025-09-03 10:49:15 -0400

    build fix

--- ee/server/src/app/msp/extensions/[id]/ExtensionIframe.tsx
+++ ee/server/src/app/msp/extensions/[id]/ExtensionIframe.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useMemo, useRef } from 'react';
-import { bootstrapIframe } from '@ee/lib/extensions/ui';
+import { bootstrapIframe } from '@ee/lib/extensions/ui/iframeBridge';
 
 type Props = {
   domain: string; // e.g. ext-abc.tenant.example.com
