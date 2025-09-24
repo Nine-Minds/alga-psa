79c5bd8d1202c6fe33e1bd219ca5b69f3508237a
Robert Isaacs <robert@nineminds.com>
2025-09-24 03:02:09 +0000

    webpack / turbopack harmony / fixed missing client portal settings

--- server/src/empty/components/settings/general/ClientPortalSettings.tsx
+++ server/src/empty/components/settings/general/ClientPortalSettings.tsx
@@ -3,6 +3,10 @@
 import React from 'react';
 
 const ClientPortalSettings: React.FC = () => {
+  if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line no-console
+    console.warn('[CE stub ClientPortalSettings] rendered');
+  }
   return null;
 };
 
