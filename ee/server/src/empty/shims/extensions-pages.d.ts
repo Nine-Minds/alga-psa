09e15fef30fa03918d2bce1d3ef2efa3e9896553
Robert Isaacs <robert@nineminds.com>
2025-10-09 17:43:07 -0400

    refactor: update LoginPage navigation method to accept custom login path fix: change import path for getServerSession to use auth-compat feat: add TypeScript declarations for product extensions pages feat: add TypeScript augmentation for NextRequest interface chore: reorganize tsconfig paths and include/exclude settings

---
+++ ee/server/src/empty/shims/extensions-pages.d.ts
@@ -0,0 +1,15 @@
+declare module '@product/extensions/pages/details' {
+  const Component: any;
+  export default Component;
+}
+
+declare module '@product/extensions/pages/settings' {
+  const Component: any;
+  export default Component;
+}
+
+declare module '@product/extensions/pages/list' {
+  const Component: any;
+  export default Component;
+}
+
