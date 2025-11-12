42d1aa28105a1691ac4191f6df768da1597c07da
Robert Isaacs <robert@nineminds.com>
2025-11-12 11:23:52 -0500

    unused file

--- server/tmp-check-triggers.ts
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-config();
-import { getSurveyTriggersForTenant } from './src/lib/actions/surveyActions';
-(async () => {
-  const tenantId = '8acae1f0-f00c-4942-9bb9-08bca0c2d4a5';
-  const triggers = await getSurveyTriggersForTenant(tenantId);
-  console.log('triggers', triggers);
-  process.exit(0);
-})();
