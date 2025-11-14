4a71336df9ff0cbd6d4f7fca928d6a4f907826f6
Natallia Bukhtsik <natallia@nineminds.com>
2025-11-14 16:39:20 -0500

    Revert product-chat to relative paths and add Docker stubs instead of @ee alias, since webpack aliases don't work outside server/

--- server/src/empty/services/chatCompletionsService.ts
+++
@@ -1,8 +0,0 @@
-// Community Edition stub for Chat Completions Service
-// This feature is only available in Enterprise Edition
-
-export class ChatCompletionsService {
-  constructor() {
-    throw new Error('Chat completions service is only available in Enterprise Edition');
-  }
-}
