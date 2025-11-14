69c959e12cca6bdf4fc779cf5b14526425cca316
Natallia Bukhtsik <natallia@nineminds.com>
2025-11-14 16:39:20 -0500

    Revert product-chat to relative paths and add Docker stubs instead of @ee alias, since webpack aliases don't work outside server/

--- packages/product-chat/ee/entry.tsx
+++ packages/product-chat/ee/entry.tsx
@@ -2,15 +2,15 @@
 // This will import the actual implementation from the ee/ directory
 
 // Chat service exports
-export { ChatStreamService } from '@ee/services/chatStreamService';
-export { TemporaryApiKeyService } from '@ee/services/temporaryApiKeyService';
-export { ChatCompletionsService } from '@ee/services/chatCompletionsService';
+export { ChatStreamService } from '../../../ee/server/src/services/chatStreamService';
+export { TemporaryApiKeyService } from '../../../ee/server/src/services/temporaryApiKeyService';
+export { ChatCompletionsService } from '../../../ee/server/src/services/chatCompletionsService';
 
 // Default export
 const chat = {
-  ChatStreamService: () => import('@ee/services/chatStreamService').then(mod => mod.ChatStreamService),
-  TemporaryApiKeyService: () => import('@ee/services/temporaryApiKeyService').then(mod => mod.TemporaryApiKeyService),
-  ChatCompletionsService: () => import('@ee/services/chatCompletionsService').then(mod => mod.ChatCompletionsService),
+  ChatStreamService: () => import('../../../ee/server/src/services/chatStreamService').then(mod => mod.ChatStreamService),
+  TemporaryApiKeyService: () => import('../../../ee/server/src/services/temporaryApiKeyService').then(mod => mod.TemporaryApiKeyService),
+  ChatCompletionsService: () => import('../../../ee/server/src/services/chatCompletionsService').then(mod => mod.ChatCompletionsService),
 };
 
 export default chat;
