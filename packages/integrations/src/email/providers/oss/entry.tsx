14cd069a77a4f5cdd630e0d25a954d64b22fcd29
Robert Isaacs <robert@nineminds.com>
2026-01-17 19:25:18 -0500

    Export EmailProviderConfiguration from integrations entry

--- packages/integrations/src/email/providers/oss/entry.tsx
+++ packages/integrations/src/email/providers/oss/entry.tsx
@@ -1,3 +1,4 @@
 export { MicrosoftProviderForm } from '../../../../../../server/src/components/MicrosoftProviderForm';
 export { GmailProviderForm } from '../../../../../../server/src/components/GmailProviderForm';
 export { ImapProviderForm } from '../../../../../../server/src/components/ImapProviderForm';
+export { EmailProviderConfiguration } from '../../../../../../server/src/components/EmailProviderConfiguration';
