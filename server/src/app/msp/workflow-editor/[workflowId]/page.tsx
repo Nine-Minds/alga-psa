49212fc3b97fc59486076a1e1a0fec961d33ced2
Robert Isaacs <robert@nineminds.com>
2026-02-22 11:13:44 -0500

    Fix workflow editor route param loading

--- server/src/app/msp/workflow-editor/[workflowId]/page.tsx
+++ server/src/app/msp/workflow-editor/[workflowId]/page.tsx
@@ -1,17 +1,19 @@
 import WorkflowAutomationGate from '../../_components/WorkflowAutomationGate';
 
 interface WorkflowEditorPageProps {
-  params: {
+  params: Promise<{
     workflowId: string;
-  };
+  }>;
 }
 
-export default function WorkflowEditorPage({ params }: WorkflowEditorPageProps) {
+export default async function WorkflowEditorPage({ params }: WorkflowEditorPageProps) {
+  const resolvedParams = await params;
+
   return (
     <WorkflowAutomationGate
       workflowProps={{
         mode: 'editor-designer',
-        workflowId: params.workflowId
+        workflowId: resolvedParams.workflowId
       }}
     />
   );
