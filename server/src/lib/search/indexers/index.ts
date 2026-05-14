99dd538ceafc6d10362a963842da88e4dce97390
Natallia Bukhtsik <natallia@nineminds.com>
2026-05-13 22:02:46 -0400

    feat(F025): index tickets for app search

--- server/src/lib/search/indexers/index.ts
+++ server/src/lib/search/indexers/index.ts
@@ -2,6 +2,7 @@ import type { EntityIndexer } from '../types';
 
 import { clientIndexer } from './client';
 import { contactIndexer } from './contact';
+import { ticketIndexer } from './ticket';
 import { userIndexer } from './user';
 
-export const ceIndexers: EntityIndexer[] = [clientIndexer, contactIndexer, userIndexer];
+export const ceIndexers: EntityIndexer[] = [clientIndexer, contactIndexer, userIndexer, ticketIndexer];
