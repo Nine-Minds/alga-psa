94965f79d16e9e32cc9c9584514ff5fb450eb47c
Natallia Bukhtsik <natallia@nineminds.com>
2026-05-13 22:04:22 -0400

    feat(F026): index ticket comments for app search

--- server/src/lib/search/indexers/index.ts
+++ server/src/lib/search/indexers/index.ts
@@ -2,7 +2,14 @@ import type { EntityIndexer } from '../types';
 
 import { clientIndexer } from './client';
 import { contactIndexer } from './contact';
+import { ticketCommentIndexer } from './ticket_comment';
 import { ticketIndexer } from './ticket';
 import { userIndexer } from './user';
 
-export const ceIndexers: EntityIndexer[] = [clientIndexer, contactIndexer, userIndexer, ticketIndexer];
+export const ceIndexers: EntityIndexer[] = [
+  clientIndexer,
+  contactIndexer,
+  userIndexer,
+  ticketIndexer,
+  ticketCommentIndexer,
+];
