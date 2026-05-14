b9b18988db45fabfef452ce3ca32ba0b8d8f5015
Natallia Bukhtsik <natallia@nineminds.com>
2026-05-13 22:01:27 -0400

    feat(F024): index internal users for app search

--- server/src/lib/search/indexers/index.ts
+++ server/src/lib/search/indexers/index.ts
@@ -2,5 +2,6 @@ import type { EntityIndexer } from '../types';
 
 import { clientIndexer } from './client';
 import { contactIndexer } from './contact';
+import { userIndexer } from './user';
 
-export const ceIndexers: EntityIndexer[] = [clientIndexer, contactIndexer];
+export const ceIndexers: EntityIndexer[] = [clientIndexer, contactIndexer, userIndexer];
