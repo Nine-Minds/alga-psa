964c67be64a8a6fc5c3b232077fdf48cd7e45421
Robert Isaacs <robert@nineminds.com>
2026-09-08 08:25:30 -0400

    refactor(workflow): isolate inference algorithm from shared edition adapters

--- packages/ee/src/services/workflowInferenceService.ts
+++ packages/ee/src/services/workflowInferenceService.ts
@@ -1,8 +1,8 @@
-import { createWorkflowInferenceService } from '../../../../shared/workflow/services/workflowInferenceService';
+import { createWorkflowInferenceService } from '../../../../packages/workflow-inference/src/index';
 import { resolveChatProvider } from './chatProviderResolver';
 import { toAiCreditsError } from '../lib/aiGateway/errors';
 import { notifyAiCreditsUnavailable } from '../lib/aiGateway/notifications';
 
-export { WorkflowInferenceServiceError } from '../../../../shared/workflow/services/workflowInferenceService';
-export type { WorkflowStructuredOutputRequest } from '../../../../shared/workflow/services/workflowInferenceService';
+export { WorkflowInferenceServiceError } from '../../../../packages/workflow-inference/src/index';
+export type { WorkflowStructuredOutputRequest } from '../../../../packages/workflow-inference/src/index';
 export const inferWorkflowStructuredOutput = createWorkflowInferenceService({ resolveChatProvider, toAiCreditsError, notifyAiCreditsUnavailable });
